044b597ff81975616ca4136120aaba9fb4d62236
exsella <rahelexsella@gmail.com>
2026-08-03 18:43:57 +0700

    Update Product API

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ProductController;
 
 Route::get('/product', [ProductController::class, 'index'])->name('product');
+Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+Route::put('/product/{product}', [ProductController::class, 'update'])->name('product.update');
